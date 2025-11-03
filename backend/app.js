b16f64eea46cfd0d3b61dd5d9f833d4446209795
Sam Maister <goosewobbler@protonmail.com>
2025-11-03 23:20:20 +0000

    fix: hook up CORS correctly

--- backend/app.js
+++ backend/app.js
@@ -4,10 +4,13 @@ const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const fileUpload = require("express-fileupload");
 
-const app = express();
-
 // config
 require('dotenv').config();
+
+const { corsPolicy } = require("./config/cors");
+const app = express();
+
+app.use(corsPolicy);
 app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
--- backend/config/cors.js
+++ backend/config/cors.js
@@ -3,7 +3,7 @@ const { env } = require("./env");
 
 const corsPolicy = cors({
   origin: env.UI_URL,
-  method: ["GET", "POST", "PUT", "DELETE"],
+  method: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Accept", "Origin", "X-CSRF-TOKEN"],
   credentials: true,
 });
